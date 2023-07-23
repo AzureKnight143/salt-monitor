0a0f7d32965ce9cd14886e9069b803caa1a129eb
Matt Wright <azureknight143@gmail.com>
2023-07-23 17:48:18 -0400

    feat: added deep sleep

--- salt-monitor.ino
+++ salt-monitor.ino
@@ -6,10 +6,12 @@ UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 void setup() {
   Serial.begin(9600);
+  delay(1000);
 }
 
 void loop() {
   float distance = distanceSensor.measureDistanceCm();
   Serial.println(distance);
+  ESP.deepSleep(5e6);
   delay(1000);
 }
