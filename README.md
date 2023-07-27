4fbbbe1908e753e702ed4091c210e5b50d17a248
Matt Wright <azureknight143@gmail.com>
2023-07-27 14:41:46 -0400

    feat: added wifi connection

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # salt-monitor
- 
+
+## Setup
+
+Create an `arduino_secrets.h` file and add the following:
+
+```
+#define SECRET_WIFI_SSID "YOUR SSID"
+#define SECRET_WIFI_PASSWORD "YOUR PASSWORD"
+```
--- salt-monitor.ino
+++ salt-monitor.ino
@@ -1,4 +1,9 @@
 #include <HCSR04.h>
+#include <ESP8266WiFi.h>
+#include "arduino_secrets.h"
+
+const char wifiSSID[] = SECRET_WIFI_SSID;
+const char wifiPassword[] = SECRET_WIFI_PASSWORD;
 
 const byte triggerPin = D4;
 const byte echoPin = D3;
@@ -6,12 +11,31 @@ UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 void setup() {
   Serial.begin(9600);
+  //Serial.setDebugOutput(true);
   delay(1000);
+  Serial.println();
+  connectToWifi();
 }
 
 void loop() {
   float distance = distanceSensor.measureDistanceCm();
   Serial.println(distance);
-  ESP.deepSleep(5e6);
+  //ESP.deepSleep(5e6);
   delay(1000);
+}
+
+void connectToWifi() {
+  WiFi.setPhyMode(WIFI_PHY_MODE_11G);
+  WiFi.begin(wifiSSID, wifiPassword);
+  Serial.print("Connecting to ");
+  Serial.print(wifiSSID);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 }
