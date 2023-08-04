5a1cedd542dec819be335fd62823c6406370cde4
Matt Wright <azureknight143@gmail.com>
2023-08-04 08:42:46 -0400

    feat: added error message

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # salt-monitor
 
+A distance monitor for my water softener salt tank using a Ultrasonic Distance Sensor. It does the following in a loop:
+
+- Connects to the wifi network configured in the arduino_secrets.h.
+- Scans distance until it gets a result or fails 6 times.
+- Sends distance or error to serial monitor.
+- Goes to deep sleep for the configured time, currently 5 seconds.
+
 ## Setup
 
 Create an `arduino_secrets.h` file and add the following:
@@ -8,3 +15,11 @@ Create an `arduino_secrets.h` file and add the following:
 #define SECRET_WIFI_SSID "YOUR SSID"
 #define SECRET_WIFI_PASSWORD "YOUR PASSWORD"
 ```
+
+## Pin Layout
+
+- Board D1 - Sensor Trig
+- Board D2 - Sensor Echo
+- Board 5V - Sensor Vcc
+- Board G - Sensor Gnd
+- Board D0 - Board RST (wakes board from deep sleep)
--- salt-monitor.ino
+++ salt-monitor.ino
@@ -5,23 +5,33 @@
 const char wifiSSID[] = SECRET_WIFI_SSID;
 const char wifiPassword[] = SECRET_WIFI_PASSWORD;
 
-const byte triggerPin = D4;
-const byte echoPin = D3;
+const byte triggerPin = D1;
+const byte echoPin = D2;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 void setup() {
   Serial.begin(9600);
-  //Serial.setDebugOutput(true);
   delay(1000);
   Serial.println();
   connectToWifi();
 }
 
 void loop() {
-  float distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);
-  //ESP.deepSleep(5e6);
-  delay(1000);
+  float distance = -1;
+  int scanCount = 0;
+
+  while (distance == -1 && scanCount < 6) {
+    scanCount++;
+    distance = distanceSensor.measureDistanceCm();
+    delay(1000);
+  }
+
+  if (distance == -1 && scanCount == 6) {
+    Serial.println("error");
+  } else {
+    Serial.println(distance);
+  }
+  ESP.deepSleep(5e6);
 }
 
 void connectToWifi() {
